0435c141e22d3686443f3b2f2f395140bf500a74
mayerlyuribe <mayerlyuribe@colegioguanenta.edu.co>
2026-03-12 01:09:40 -0500

    detalles finales

--- Pilita_1/pilaStack.java
+++ Pilita_1/pilaStack.java
@@ -10,8 +10,9 @@ public class pilaStack {
             String number = String.valueOf(aleatorio);
             pilaNumbers1.push(number);
         }
-        System.out.println("\n" + pilaNumbers1.toString());
-        System.out.println("------------------------");
+        System.out.println("\nPila 1 con 7 numeritos aleatorios:");
+        System.out.println(pilaNumbers1.toString());
+        System.out.println("--------------------------------------------------");
 
         Stack<String> pilaNumbers2 = new Stack<>();
 
@@ -19,8 +20,9 @@ public class pilaStack {
             pilaNumbers2.push(pilaNumbers1.peek());
             pilaNumbers1.pop();
         }
+        System.out.println("Pila 2 con los datos de la pila 1 en orden inverso:");
         System.out.println(pilaNumbers2.toString());
-        System.out.println("------------------------");
+        System.out.println("---------------------------------------------------");
 
 
     }
